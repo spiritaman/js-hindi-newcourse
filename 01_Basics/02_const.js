4beb0c19956d4d6b533e0f3c65f40b097ae5c0c0
Anant Tiwari <122080038+spiritaman@users.noreply.github.com>
2025-02-25 06:51:25 +0000

    NAME CHANGED

---
+++ 01_Basics/02_const.js
@@ -0,0 +1,14 @@
+const accountId  = 123445
+let accountEmail = "anant@gmail.com"
+var accountPassword = "anant@123"
+ accountCity = "Jabalpur"
+
+// accountId = 4145524
+
+accountEmail = "radhikapaliwal@gmail.com"
+accountPassword = "raddzo7gg"
+accountCity = "pune"
+let accountState;
+
+ console.table([ accountId, accountEmail,accountPassword,accountCity , accountState ])
+
---
+++ 01_Basics/04_conversionandoperation.js
@@ -0,0 +1,10 @@
+let score = undefined
+console.log(typeof(score));
+console.log(typeof score);
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber);
+
+console.log(ValueInNumber);
+
+
