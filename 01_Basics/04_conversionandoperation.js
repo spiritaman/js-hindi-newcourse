036f2c01233afffce3ab211a86a9a5feb549db70
Anant Tiwari <122080038+spiritaman@users.noreply.github.com>
2025-02-25 07:27:49 +0000

    conversion of datatype

--- 01_Basics/04_conversionandoperation.js
+++ 01_Basics/04_conversionandoperation.js
@@ -9,8 +9,38 @@ let ValueInNumber = Number(score)
 console.log(typeof ValueInNumber);
 console.log(ValueInNumber);
 
-// 33 = 33 (NUMBER)
-//"33abc" = NAN (Not a number)
-// true = 1 (boolean)
-// false = 0 (boolean)
+                                 
 
+/* 33 = 33 (NUMBER)
+   "33abc" = NAN (Not a number)
+   true = 1 (boolean)
+   false = 0 (boolean)
+*/
+
+
+                      
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+
+
+/* 
+  1 = true
+  0 = false
+  null = false
+  undefined = false
+  "" = false 
+  "1"=true
+  "0"=true
+*/
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+ 
+ 
