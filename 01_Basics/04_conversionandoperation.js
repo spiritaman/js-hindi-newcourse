2954905560b8b3acc15f8627bb02b1b968d75f87
Anant Tiwari <122080038+spiritaman@users.noreply.github.com>
2025-02-25 06:56:59 +0000

    done

--- 01_Basics/04_conversionandoperation.js
+++ 01_Basics/04_conversionandoperation.js
@@ -1,10 +1,16 @@
-let score = undefined
+
+let score = true
+
 console.log(typeof(score));
 console.log(typeof score);
 
 let ValueInNumber = Number(score)
-console.log(typeof ValueInNumber);
 
+console.log(typeof ValueInNumber);
 console.log(ValueInNumber);
 
+// 33 = 33 (NUMBER)
+//"33abc" = NAN (Not a number)
+// true = 1 (boolean)
+// false = 0 (boolean)
 
