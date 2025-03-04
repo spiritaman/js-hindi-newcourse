d1ac50b0a853d219f0ff77c3436bfce89bac92bb
Anant Tiwari <122080038+spiritaman@users.noreply.github.com>
2025-03-04 06:21:30 +0000

    changes done

--- 01_Basics/04_conversionandoperation.js
+++ 01_Basics/04_conversionandoperation.js
@@ -1,4 +1,6 @@
 // *********************** conversions ***********************
+
+
 let score = true
 
 console.log(typeof(score));
@@ -45,32 +47,33 @@ let stringNumber = String(someNumber)
 
 // *********************** Operations ***********************
 
+
 let value = 3
 let negValue = -value
-// console.log(negValue);
+ console.log(negValue);
 
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**3);
-// console.log(2/3);
-// console.log(2%3);
+ console.log(2+2);
+ console.log(2-2);
+ console.log(2*2);
+ console.log(2**3);
+ console.log(2/3);
+ console.log(2%3);
 
 let str1 = "hello"
 let str2 = " hitesh"
 
 let str3 = str1 + str2
-// console.log(str3);
+ console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+ console.log("1" + 2);
+ console.log(1 + "2");
+ console.log("1" + 2 + 2);
+ console.log(1 + 2 + "2");
 
-// console.log( (3 + 4) * 5 % 3);
+ console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+ console.log(+true);
+ console.log(+"");
 
 let num1, num2, num3
 
